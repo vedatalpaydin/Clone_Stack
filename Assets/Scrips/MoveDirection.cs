dcf7328e03b24bab21bbd465a56b27e02790aad2
vedatalpaydin <vedatalpaydin7@icloud.com>
2021-03-31 09:59:32 +0300

    Game is Finish

---
+++ Assets/Scrips/MoveDirection.cs
@@ -0,0 +1,1 @@
+public enum MoveDirection{X,Z}
---
+++ Assets/Scrips/ScoreText.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class ScoreText : MonoBehaviour
+{
+    private int score;
+    private TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        GameManager.OnCubeSpawned += GameManager_OnCubeSpawned;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.OnCubeSpawned -= GameManager_OnCubeSpawned;
+    }
+
+    private void GameManager_OnCubeSpawned()
+    {
+        score++;
+        text.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
